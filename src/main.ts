28c22b94c3229828c59e2e9e08bb4dde788fccc9
Jose Rubio <jadrubio@gmail.com>
2024-03-04 15:45:21 -0500

    basic create user end point with dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true // strips additional properties from requests (via DTO)
+    })
+  )
   await app.listen(3000);
 }
 bootstrap();
